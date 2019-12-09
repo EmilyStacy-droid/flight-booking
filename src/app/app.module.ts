2667a95112c94bb256d5c61df4b9cfd098d3a707
Emily Stacy <Emily.Stacy@improving.com>
2019-12-09 11:05:36 -0600

    routing built

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import * as components from './index';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    components.SearchComponent,
+    components.FlightListComponent,
+    components.BookingComponent,
+    components.FlightBookedComponent  
   ],
   imports: [
     BrowserModule,
---
+++ src/app/index.ts
@@ -0,0 +1,4 @@
+export * from "./search/search.component";
+export * from "./flight-list/flight-list.component";
+export * from "./booking/booking.component";
+export * from "./flight-booked/flight-booked.component";
